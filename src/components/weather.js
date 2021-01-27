4e725cb11b9348c47647e6fa8c956de3aeb1b723
DimonLy <lyashenkodi@gmail.com>
2021-01-27 21:01:29 +0200

    First commit

---
+++ src/components/weather.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Weather = (props) => {
+    return (
+        <div>
+            {props.city &&
+                <div>
+                    <p>Location: {props.city}, {props.country}</p>
+                    <p>Temperature: {props.temp}</p>
+                    <p>Pressure: {props.pressure}</p>
+                    <p>Sunset: {props.sunset}</p>
+                    <p>{props.error}</p>
+                </div>
+            }
+            <p>{props.error}</p>
+        </div>
+
+    );
+}
+
+export default Weather;
