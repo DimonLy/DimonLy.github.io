b8ceeec6565c9f0aa2ad9ceaacde34324df10c45
DimonLy <lyashenkodi@gmail.com>
2021-01-28 12:42:09 +0200

    Updates

---
+++ precache-manifest.9cbda609404cf0b5c671985150fabc4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e6d0aafa38518d1ee3d",
+    "url": "/DimonLy/DimonLy.github.io.git/static/css/main.4096633c.chunk.css"
+  },
+  {
+    "revision": "5e6d0aafa38518d1ee3d",
+    "url": "/DimonLy/DimonLy.github.io.git/static/js/main.5e6d0aaf.chunk.js"
+  },
+  {
+    "revision": "c129698d0ca31d928844",
+    "url": "/DimonLy/DimonLy.github.io.git/static/js/1.c129698d.chunk.js"
+  },
+  {
+    "revision": "9ffb5863f51ad4f4f81b",
+    "url": "/DimonLy/DimonLy.github.io.git/static/js/runtime~main.9ffb5863.js"
+  },
+  {
+    "revision": "41ff7cec01a22ef9dc2b2c595bfe793b",
+    "url": "/DimonLy/DimonLy.github.io.git/index.html"
+  }
+];
